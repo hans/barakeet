b558499d23e3125efc68537cca3fac97487a2de4
Jon Gauthier <jon@gauthiers.net>
2024-06-03 16:20:55 -0700

    mismatch erp updates

---
+++ src/stimuli.py
@@ -0,0 +1,15 @@
+
+
+
+
+# point of disambiguation relative to word onset
+POD_dict = {
+    # bountiful/mountains
+    'bm': 0.28,
+
+    # desolate/necessary
+    'dn': 0.295,
+
+    # beneficial/penecillin
+    'pb': 0.21,
+}
