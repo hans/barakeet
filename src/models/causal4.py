c94ff5d31a430dd0b75a091bc65efadcd5836aa3
Jon Gauthier <jon@gauthiers.net>
2025-06-15 11:17:56 -0700

    causal4: remove phase2

--- src/models/causal4.py
+++ src/models/causal4.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import StandardScaler
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
+from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import StratifiedKFold
 from sklearn.pipeline import make_pipeline
 from src.models.causal import run_phase1
@@ -16,8 +17,6 @@ class Causal4AnalysisResult:
     subject: str
     population_A: list[int]
     population_A_window: tuple[float, float]
-    population_B: list[int]
-    population_B_window: tuple[float, float]
 
     epochs: mne.Epochs
     test_trial_metadata: pd.DataFrame
@@ -31,110 +30,51 @@ class Causal4AnalysisResult:
     Test output: probability of the ground-truth phoneme
     computed from population A.
     """
-    p_gt_lexical_evidence: np.ndarray
-    """
-    Test output: probability of the ground-truth lexical evidence
-    computed from population B.
-    """
 
     phase1_val_score: float
     """Score on the data that will be passed to phase2"""
 
     phase1_test_scores: np.ndarray
-    phase2_test_scores: np.ndarray
 
     # post-init validation checks
     def __post_init__(self):
         assert len(self.p_gt_phoneme) == len(self.test_trial_metadata)
-        assert len(self.p_gt_lexical_evidence) == len(self.test_trial_metadata)
-
-
-def run_phase2(epochs, subject, phoneme_pair,
-                population_B, population_B_window,
-                metadata,
-                idxs_val, y_proba_val):
-    """
-    Run the second phase of the causal analysis.
-    """
-    # Train a lexical evidence decoder on the same set
-    idxs_train = np.array(list(set(np.arange(len(metadata))) - set(idxs_val)))
-    X = epochs[subject][f"phoneme_pair == '{phoneme_pair}'"] \
-        .pick(population_B) \
-        .copy().crop(*population_B_window) \
-        .get_data()
-    X = X.reshape(len(X), -1)
-    X_train = X[idxs_train]
-    y_train = metadata.iloc[idxs_train].lexical_evidence.values
-    X_val = X[idxs_val]
-    y_val = metadata.iloc[idxs_val].lexical_evidence.values
-
-    from sklearn.decomposition import PCA
-    from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
-    from sklearn.pipeline import make_pipeline
-    from sklearn.preprocessing import StandardScaler
-    from sklearn.model_selection import cross_val_score, StratifiedKFold
-
-    pipeline = make_pipeline(
-        StandardScaler(),
-        PCA(n_components=0.9),
-        LogisticRegressionCV(Cs=5, max_iter=1000, cv=StratifiedKFold(2, shuffle=True),
-                             class_weight="balanced"),
-    )
-    test_scores = cross_val_score(pipeline, X_train, y_train, cv=StratifiedKFold(2, shuffle=True))
-    model = pipeline.fit(X_train, y_train)
-    assert list(model.classes_) == [0, 1]
-
-    # # Retain only trials with mismatch
-    # keep_idxs = [idx for idx, idx_is_mismatch in zip(idxs_val, metadata.iloc[idxs_val].mismatch)
-    #              if idx_is_mismatch == 1]
-    keep_idxs = np.arange(len(idxs_val))
-
-    p_gt_phoneme = y_proba_val[np.arange(len(keep_idxs)),
-                               metadata.iloc[keep_idxs].lexical_evidence]
-    p_gt_lexical_evidence = model.predict_proba(X_val) \
-        [np.arange(len(keep_idxs)),
-         metadata.iloc[keep_idxs].lexical_evidence]
-
-    return test_scores, p_gt_phoneme, p_gt_lexical_evidence
 
 
 def run_causal4_analysis(epochs, subject, phoneme_pair,
-                            population_A, population_A_window,
-                            population_B, population_B_window,
-                            split_strategy="stratify",
-                            pca_num_components=32):
-    # phase1
+                         population_A, population_A_window,
+                         split_strategy="stratify",
+                         pca_num_components=32):
+    # phase1: train a GT phoneme decoder on population A
     fitted, metadata, idxs_val, y_proba_val = run_phase1(
         epochs, subject, phoneme_pair,
         population_A, population_A_window,
         split_strategy=split_strategy,
     )
-    from sklearn.metrics import roc_auc_score
+    
+    # estimate ROC-AUC on validation set
     roc_auc = roc_auc_score(
         metadata.iloc[idxs_val].categorical_acoustic_cue.combine(0, max),
         y_proba_val[:, 1],
     )
 
-    test_scores, p_gt_phoneme, p_gt_lexical_evidence = run_phase2(
-        epochs, subject, phoneme_pair,
-        population_B, population_B_window,
-        metadata,
-        idxs_val, y_proba_val
-    )
+    # get p(gt phoneme) on the validation set predictions. class 0 of the
+    # decoder corresponds to the left phoneme (just when `lexical_evidence == 0`)
+    # and class 1 corresponds to the right phoneme (just when `lexical_evidence == 1`)
+    # so we can use the `lexical_evidence` column to index into the probabilities
+    # predicted by the phoneme decoder
+    p_gt_phoneme = y_proba_val[np.arange(len(idxs_val)),
+                               metadata.iloc[idxs_val].lexical_evidence]
 
     return Causal4AnalysisResult(
         subject=subject,
         population_A=population_A,
         population_A_window=population_A_window,
-        population_B=population_B,
-        population_B_window=population_B_window,
 
         epochs=epochs[subject],
         test_trial_metadata=metadata.iloc[idxs_val],
 
         p_gt_phoneme=p_gt_phoneme,
-        p_gt_lexical_evidence=p_gt_lexical_evidence,
         phase1_val_score=roc_auc,
         phase1_test_scores=fitted['test_score'],
-        phase2_test_scores=test_scores,
     )
