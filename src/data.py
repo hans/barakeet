be1ebd223ec12dbafe952802b8ab3394c89d6e3e
Jon Gauthier <jon@gauthiers.net>
2025-07-22 16:20:19 -0700

    get_electrode_df: add MNI coordinates

--- src/data.py
+++ src/data.py
@@ -12,9 +12,10 @@ IMAGING_PATH = "/data_store2/imaging/subjects"
 
 def get_electrode_df(subject: str) -> pd.DataFrame:
     electrode_path = Path(IMAGING_PATH) / subject / "elecs" / "TDT_elecs_all.mat"
-    elecs = loadmat(electrode_path, simplify_cells=True)["anatomy"]
-    ret = pd.DataFrame(elecs, columns=["electrode_name", "long_name", "type", "roi"]) \
-        .set_index("electrode_name", append=True)
+    elecs = loadmat(electrode_path, simplify_cells=True)
+    ret = pd.DataFrame(elecs["anatomy"], columns=["electrode_name", "long_name", "type", "roi"])
+    ret = pd.concat([ret, pd.DataFrame(elecs["elecmatrix"], columns=["x", "y", "z"])], axis=1)
+    ret = ret.set_index("electrode_name", append=True)
     ret.index.set_names("electrode_idx", level=0, inplace=True)
     return ret
 
