81cda38f1dea599a976905cf57dcd1c45c4eb00e
Jon Gauthier <jon@gauthiers.net>
2026-05-19 07:41:22 -0700

    pair notebook

--- notebooks/causal46_joined/as_reconciliation.py
+++ notebooks/causal46_joined/as_reconciliation.py
@@ -1,10 +1,12 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: py:percent
+#     formats: ipynb,py:percent
 #     text_representation:
 #       extension: .py
 #       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.1
 # ---
 
 # %% [markdown]
